017e9221d81cb7f31497c9f742949f0adc692e1d
Renato Groffe <renato.groffe@yahoo.com.br>
2023-01-14 11:37:30 -0300

    .NET 7 + Console App + Testes com Nanossegundos e Microssegundos
    .NET 7 + Console App + Testes com Nanossegundos e Microssegundos

---
+++ ExemploNanoMicrossegundos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+
+Console.WriteLine("***** Testes com .NET 7 | Nanossegundos e Microssegundos *****");
+Console.WriteLine($"Versao do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName}- Kernel: {Environment
+    .OSVersion.VersionString}");
+
+var momento1 = DateTime.Now;
+ExibirInfoMomento(momento1, "Primeiro momento");
+
+Console.WriteLine();
+Console.WriteLine("Aguardar 500 mil microssegundos...");
+Thread.Sleep(TimeSpan.FromMicroseconds(500_000));
+
+var momento2 = DateTime.Now;
+ExibirInfoMomento(momento2, "Segundo momento");
+
+void ExibirInfoMomento(DateTime momento, string mensagem)
+{
+    Console.WriteLine();
+    Console.WriteLine($"--- {mensagem} ---");
+    Console.WriteLine($"Horario: {momento.Hour:00}:{momento.Minute:00}:{momento.Second:00}");
+    Console.WriteLine($"  + Milissegundos: {momento.Millisecond}");
+    Console.WriteLine($"  + Nanossegundos: {momento.Nanosecond}");
+    Console.WriteLine($"  + Microssegundos: {momento.Microsecond}");
+}
